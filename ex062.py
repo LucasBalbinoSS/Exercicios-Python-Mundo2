cab79707dba1523076fe573c89ad916458d5230e
Lucas Balbino S. Silva <lucas.silva844@fatec.sp.gov.br>
2021-05-10 22:30:49 -0300

    python exercises

--- ex062.py
+++ ex062.py
@@ -1,3 +1,4 @@
+print('-=' * 10)
 print('Gerador de P.A')
 print('-=' * 10)
 
@@ -5,9 +6,16 @@ primeiro = int(input('Informe o primeiro termo (de onde a progressão começará
 razão = int(input('Informe a razão: '))
 termo = primeiro
 c = 1
+total = 0
+mais = 10
 
-while c <= 10:
-    print('%i - ' % termo, end='')
-    termo += razão
-    c += 1
-print('\033[1;31mFIM')
+
+while mais != 0:
+    total += mais
+    while c <= total:
+        print('%i - ' % termo, end='')
+        termo += razão
+        c += 1
+    print('\033[1;31mPausa...\033[m')
+    mais = int(input('Deseja ver mais quantos termos?: '))
+print('Progressão finalizada com %i termos' % total)
