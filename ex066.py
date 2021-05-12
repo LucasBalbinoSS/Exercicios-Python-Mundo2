e85ba6bf05c48f519eee6d337251c11aa516200f
Lucas Balbino S. Silva <lucas.silva844@fatec.sp.gov.br>
2021-05-11 23:51:38 -0300

    python exercise

---
+++ ex066.py
@@ -0,0 +1,11 @@
+soma = 0
+contador = 0
+
+while True:
+    num = int(input('Digite um número (\033[1;31m999\033[m para \033[4mencerrar\033[m o programa): '))
+    if num == 999:
+        break
+    soma += num
+    contador += 1
+
+print(f'Foram digitados {contador} números, e a soma de todos esses {contador} é igual a {soma}')
