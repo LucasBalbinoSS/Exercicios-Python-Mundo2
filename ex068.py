2b8b246408254ca284d52c2afd36b9d97fa416db
Lucas Balbino S. Silva <lucas.silva844@fatec.sp.gov.br>
2021-05-19 12:09:10 -0300

    python exercise

--- ex068.py
+++ ex068.py
@@ -1,5 +1,11 @@
 from random import randint
 
+print('=' * 40)
+print(' ' * 12, 'PAR OU ÍMPAR')
+print('=' * 40)
+
+contadorvitorias = 0
+
 while True:
     jogador = int(input('Informe um valor: '))
     while jogador > 10 or jogador < 0:
@@ -7,17 +13,22 @@ while True:
 
     computador = randint(0, 10)
     total = jogador + computador
+
     escolha = str(input('Você quer par ou ímpar? [ P / I ] ')).strip().upper()
+    while escolha != 'P' and escolha != 'I':
+        escolha = str(input('Você quer par ou ímpar? [ P / I ] ')).strip().upper()
 
     print()
 
     if total % 2 == 0 and escolha == 'P':
+        contadorvitorias += 1
         print('=' * 27)
         print('Parabéns, você ganhou da máquina!')
         print(f'O computador escolheu {computador}, e você {jogador}. Total de {total}')
         print('=' * 27)
 
     elif total % 2 == 1 and escolha == 'I':
+        contadorvitorias += 1
         print('=' * 27)
         print('Parabéns, você ganhou da máquina!')
         print(f'O computador escolheu {computador}, e você {jogador}. Total de {total}')
@@ -30,3 +41,4 @@ while True:
         break
 
 print(f'Você jogou {jogador} e o computador {computador}. Total de {total}')
+print(f'Você venceu {contadorvitorias} vezes!')
