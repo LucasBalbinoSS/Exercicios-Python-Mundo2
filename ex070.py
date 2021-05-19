9f42b90b3b75aa3061ae1651e81a22fd0dc920c7
Lucas Balbino S. Silva <lucas.silva844@fatec.sp.gov.br>
2021-05-19 12:52:10 -0300

    python exercises

--- ex070.py
+++ ex070.py
@@ -3,21 +3,21 @@ print(' ' * 10, 'LOJA BALBINO')
 print('=-' * 20)
 print()
 
-soma = 0.0
-contador = 0
-menorpreco = 0.0
-nomemenorpreco = ''
-
+soma = contador = totmil = menorpreco = 0
+nomemenorpreco = ' '
 
 while True:
     nomeproduto = str(input('Informe o nome do produto: ')).strip().title()
     precoproduto = float(input('Informe o preço do produto: R$ '))
+    contador += 1
     if precoproduto > 1000:
-        contador += 1
-    elif precoproduto < menorpreco:
-        nomemenorpreco = nomeproduto
-        menorpreco += precoproduto
+        totmil += 1
+
     soma += precoproduto
+
+    if contador == 1 or precoproduto < menorpreco:
+        menorpreco = precoproduto
+        nomemenorpreco = nomeproduto
 
     continuar = str(input('Quer continuar? [ S / N ]: ')).strip().upper()
     if continuar == 'N':
@@ -25,7 +25,9 @@ while True:
     while continuar != 'S':
         continuar = str(input('Quer continuar? [ S / N ]: ')).strip().upper()
     print('=-' * 20)
-
+print()
+print('TOTAL DA COMPRA')
+print()
 print(f'O total gasto nessa compra foi de R${soma}')
-print(f'Você comprou {contador} produtos que custam mais de R$1000.00')
+print(f'Você comprou {totmil} produtos que custam mais de R$1000.00')
 print(f'O produto mais barato que foi coprado foi {nomemenorpreco}, e custa R${menorpreco}')
